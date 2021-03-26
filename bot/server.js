3d70fa870a0276f8cae0e69b667f613b1f3fe0c5
Paco <arcegarcia@MacBook-Pro-de-Paco.local>
2021-03-26 04:08:51 -0600

    plantilla para bot

--- bot/server.js
+++ bot/server.js
@@ -1,31 +1,38 @@
-require('dotenv').config()
-const Telegraf = require('telegraf')
-const express = require('express')
-const expressApp = express()
+require('dotenv').config(); 
+const Telegraf = require('telegraf'); 
+const express = require('express'); 
+const expressApp = express(); 
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-console.log(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN); 
+console.log(process.env.BOT_TOKEN); 
 
-expressApp.use(bot.webhookCallback('/bot'))
-bot.telegram.setWebhook('https://bot-speedtest.vercel.app/bot')
+expressApp.use(bot.webhookCallback('/bot')); 
+bot.telegram.setWebhook('https://bot-speedtest.vercel.app/bot'); 
 
 expressApp.get('/', (req, res) => {
-  res.send('Bot version 1')
+    res.send('Bot version 1');
 })
 
 
-bot.command('ver', (ctx) =>{
-  ctx.reply(`${Math.floor(Math.random() * 100)}`)
+bot.command('ver', async (ctx) => {
+    const res = await fetchVelocidades();
+    ctx.reply(res);
 })
 
 
-bot.on('text', ctx => {
-  if (ctx.message.text.includes('ver')) {
-      
-  }
-  
-})
+bot.on('text', async (ctx) => {
+    if (ctx.message.text.includes('ver')) {
+        const res = await fetchVelocidades();
+        ctx.reply(res);
+    } else {
+        ctx.reply('adios test');
+    }
+});
+
+async function fetchVelocidades() {
+    return 'Hola test';
+}
 
 expressApp.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port ' + PORT);
+});
