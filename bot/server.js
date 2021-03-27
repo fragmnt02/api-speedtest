1043c53e652d42417fe087ee45ef73e162f670bb
Paco <arcegarcia@MacBook-Pro-de-Paco.local>
2021-03-26 18:31:58 -0600

    nueva estructura

--- bot/server.js
+++ bot/server.js
@@ -1,31 +1,35 @@
-require('dotenv').config(); 
-const {Telegraf} = require('telegraf'); 
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
 const express = require('express');
-const expressApp = express(); 
+const expressApp = express();
 const PORT = process.env.PORT || 3000;
-const bot = new Telegraf(process.env.BOT_TOKEN); 
+const bot = new Telegraf(process.env.BOT_TOKEN);
+console.log(BOT_TOKEN);
 
 expressApp.use(express.json());
-expressApp.post('/webhook',(req,res)=>{
-    const {chatId} = req.query;
-    const {response} = req.body;
+expressApp.post('/webhook', (req, res) => {
+    const { chatId } = req.query;
+    const { response } = req.body;
     if (chatId) {
-        bot.telegram.sendMessage(chatId,response);
+        bot.telegram.sendMessage(chatId, response);
     } else {
         console.log('NO CHAT ID PROVIDED');
     }
     res.status(200).send();
 });
 
-expressApp.use(bot.webhookCallback('/bot')); 
-bot.telegram.setWebhook('https://bot-speedtest.vercel.app/bot'); 
+expressApp.use(bot.webhookCallback('/bot'));
+bot.telegram.setWebhook('https://bot-speedtest.vercel.app/bot');
 
+bot.telegram.getMe().then((botInfo) => {
+    app.options.username = botInfo.username
+});
 
 expressApp.get('/', (req, res) => {
     res.send('Bot version 1');
 });
 
-bot.command('chat',(ctx)=>{
+bot.command('chat', (ctx) => {
     ctx.reply(ctx.message.chat.id);
 })
 
